06715140a4ff888df01f4f8865a7b3e773ed6880
Nay Myo Lwin <naymyolwin@Nays-MacBook-Pro.local>
2023-02-18 19:40:03 +0700

    input filtered for only numbers

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
-$(document).ready(
+$(document).ready(function () {
   $("#btn-start").click(function () {
-    $(".startMode").hide();
-    $(".gameMode").show();
+    $("#answerInput").val("");
+    $(".startMode").hide(400);
     $(".answerInput").focus();
-  })
-);
+    startGame();
+  });
+
+  $("#answerInput").on("keypress", function (e) {
+    if (e.which == 13) {
+      console.log("ENTER");
+      $("#answerInput").val("");
+    }
+  });
+
+  $("#answerInput").on("input", function () {
+    let input = this.value.split("");
+    if (input.length > 0) {
+      if (!/^\d+$/.test(input[input.length - 1])) {
+        input.pop();
+      }
+      $("#answerInput").val(input.join(""));
+    }
+  });
+
+  // END OF DOCUMENT READY
+});
+
+function startGame() {
+  let numbers = generateQA();
+  let question = numbers[0] + " + " + numbers[1] + " = ";
+  $("#question").text(question);
+}
+
+function generateQA() {
+  let num1 = 0;
+  let num2 = 0;
+
+  num1 = Math.floor(Math.random() * 9);
+  num2 = Math.floor(Math.random() * 9);
+
+  console.log([num1, num2]);
+  return [num1, num2];
+}
